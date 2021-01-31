6c329aa284c1658e721997925f451e16d339f239
Amy Lee <canvesgee@gmail.com>
2021-01-31 04:06:20 -0800

    added registration functions

--- sewing/urls.py
+++ sewing/urls.py
@@ -4,6 +4,8 @@ urlpatterns = [
     path('', views.index),
     path('register', views.register, name='register'),
     path('handle_registration', views.handle_registration),
-    path('sign_up', views.sign_up),
+    path('secure', views.secure),
+    # path('handle_login', views.handle_login),
+    # path('handle_logout', views.handle_logout),
     path('pay_page', views.pay_page, name='pay_page'),
 ]
--- sewing/views.py
+++ sewing/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render, redirect
+from django.contrib import messages
+import bcrypt
+from .models import *
 # from django.core.mail import send_mail
-# import bcrypt
+
 
 def index(request):
     # send_mail('from Django', 'This is an automated message from Amy', 'canvesgee@gmail.com', ['amy.star.lee@gmail.com'], fail_silently=False)
@@ -9,20 +12,37 @@ def index(request):
 def register(request):
     return render(request, "register.html")
 
-def sign_up(request):
+def pay_page(request):
+    return render(request, "pay_page.html")
+
+def secure(request):
+    return render(request, "secure.html")
+
+#POST
+
+def handle_registration(request):
+    if request.method == 'POST':
+
+        errors = User.objects.user_validator(request.POST)
+        if len(errors) > 0:
+            for error in errors:
+                messages.error(request, errors[error])
+            return redirect ('/register')
+        #codingdojo 46:49
+    
+        hashed_password = bcrypt.hashpw(request.POST['password'].encode(), bcrypt.gensalt(8)).decode()
+        new_user = User.objects.create(username = request.POST['username'], password = hashed_password)
+
+        return redirect('/secure')
+    return redirect('/register')
+
+
+
+def handle_login(request):
     print(request.POST)
     password = request.POST['password']
     # pw_hash = bcrypt.hashpw(password.encode(), bcrypt.gensalt()).decode()
     # print(pw_hash)
     # User.objects.create(username=request.POST['username'], password=pw_hash)
     return redirect('pay_page')
-
-def pay_page(request):
-    return render(request, "pay_page.html")
-
-#POST
-
-def handle_registration(request):
-    pass
-
 # Create your views here.
